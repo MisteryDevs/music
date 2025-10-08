cbfba6c6704a4100528ce7f355d538c5271eda12
MisteryDevs <officialbotdev@gmail.com>
2025-10-09 05:23:57 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -64,11 +64,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="ʏᴛ-ᴀᴘɪ", callback_data=f"oapi"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}")],
  
- [InlineKeyboardButton(text="<- 20s", callback_data=f"cseek_20"),
-
-
-InlineKeyboardButton(text="20s + >", callback_data=f"seek_20"),
-         ],
        [   InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
@@ -86,12 +81,6 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="ʏᴛ-ᴀᴘɪ", callback_data=f"oapi"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}")],
  
-[InlineKeyboardButton(text="<- 20s", callback_data=f"cseek_20"),
-
-
-InlineKeyboardButton(text="20s + >", callback_data=f"seek_20"),
-         ],
-  
        [   InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
